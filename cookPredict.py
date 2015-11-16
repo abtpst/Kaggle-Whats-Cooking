2cc42a126de648d31ad4e8486efb275fcd568827
Abhijit Tomar <abhijit141088@gmail.com>
2015-11-16 11:01:34 -0500

    Update cookPredict.py

--- cookPredict.py
+++ cookPredict.py
@@ -1,10 +1,27 @@
-def main():
+import pandas as pd
 
-    # just load the best parameters from the training script
-    
+import pickle
+import re
+from sklearn.cross_validation import train_test_split
+from sklearn.metrics.metrics import classification_report, accuracy_score, confusion_matrix
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+from sklearn.linear_model.logistic import LogisticRegression
+from sklearn.pipeline import Pipeline
+from sklearn.grid_search import GridSearchCV
+
+def main():
+   
     bestParameters = pickle.load(open("../../picks/bestParams.pkl","rb"))
     
-    # create optimal pipeline
+    traindf = pd.read_json('../../data/train.json')
+    
+    traindf['ingredients_string'] = [' '.join([WordNetLemmatizer().lemmatize(re.sub('[^A-Za-z]', ' ', line)) for line in lists]).strip() for lists in traindf['ingredients']]       
+
+    X, y = traindf['ingredients_string'], traindf['cuisine'].as_matrix()
+    
+    Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, trainSize=0.7)
     
     pip = Pipeline([
     ('vect', TfidfVectorizer(
@@ -18,26 +35,10 @@ def main():
     ('clf', LogisticRegression(C=bestParameters['clf__C']))
     ])
     
-    # still need to have a parameters map due to syntax. so just leave it empy this time
-    
     parameters = {}
     
-    # prepare training data so that the pipeline can be trained
-    
-    traindf = pd.read_json('../../data/train.json')
-    
-    traindf['ingredients_string'] = [' '.join([WordNetLemmatizer().lemmatize(re.sub('[^A-Za-z]', ' ', line)) for line in lists]).strip() for lists in traindf['ingredients']]       
-
-    X, y = traindf['ingredients_string'], traindf['cuisine'].as_matrix()
-    
-    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7)
-    
-    # train optimal pipeline
-    
-    grid_searchTS = GridSearchCV(pip,parameters,n_jobs=3, verbose=1, scoring='accuracy')
-    grid_searchTS.fit(X_train, y_train)
-    
-    # no need to do this, but still can be used to ensure that the pipeline we created is indeed the optimal one
+    gridSearchTS = GridSearchCV(pip,parameters,n_jobs=3, verbose=1, scoring='accuracy')
+    gridSearchTS.fit(X_train, y_train)
     
     predictions = grid_searchTS.predict(X_test)
     
@@ -45,19 +46,17 @@ def main():
     print ('Confusion Matrix:', confusion_matrix(y_test, predictions))
     print ('Classification Report:', classification_report(y_test, predictions))
     
-    # now prepare the test data
-    
     testdf = pd.read_json("../../data/test.json") 
    
     testdf['ingredients_string'] = [' '.join([WordNetLemmatizer().lemmatize(re.sub('[^A-Za-z]', ' ', line)) for line in lists]).strip() for lists in testdf['ingredients']]       
 
     predictions=grid_searchTS.predict(testdf['ingredients_string'])
     
-    add a new column and populate as per the predictions we got
-    
     testdf['cuisine'] = predictions
     
     print(testdf.info())
     
-    # save as csv and submit to Kaggle
     testdf.to_csv("submission.csv",index=False,columns=['id','cuisine'])
+    
+if __name__ == '__main__':
+    main()
